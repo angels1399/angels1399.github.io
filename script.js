413e41946d3b6885d3d8e235bb3b7458b7f75ab1
angels1399 <angelsaldana241@gmail.com>
2025-02-03 23:10:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function changeShoe(shoeNumber) {
-    let shoeImages = {
-        1: "https://via.placeholder.com/200/FF4500/FFFFFF?text=Nike+Red+New",
-        2: "https://via.placeholder.com/200/4169E1/FFFFFF?text=Nike+Blue+New",
-        3: "https://via.placeholder.com/200/32CD32/FFFFFF?text=Nike+Green+New"
-    };
 
-    document.getElementById("shoe" + shoeNumber).src = shoeImages[shoeNumber];
-}
