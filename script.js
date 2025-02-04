b1921e737e56d3a1846022e324e9fdc8ae4fa63a
angels1399 <angelsaldana241@gmail.com>
2025-02-03 23:01:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function showMessage() {
-    document.getElementById("message").innerText = "Hello! You made an interactive website!";
+function changeShoe(shoeNumber) {
+    let shoeImages = {
+        1: "https://via.placeholder.com/200/FF4500/FFFFFF?text=Nike+Red+New",
+        2: "https://via.placeholder.com/200/4169E1/FFFFFF?text=Nike+Blue+New",
+        3: "https://via.placeholder.com/200/32CD32/FFFFFF?text=Nike+Green+New"
+    };
+
+    document.getElementById("shoe" + shoeNumber).src = shoeImages[shoeNumber];
 }
